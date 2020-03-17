3bff12123ceba88fccbc694fe30adbdaead04212
Kevin <kevin.ng@online-pajak.com>
2020-03-17 18:40:07 +0800

    refactored FooBarQix

--- FooBarQix/src/main/java/com/foobarqix/foobarqix/FooBarQix.java
+++ FooBarQix/src/main/java/com/foobarqix/foobarqix/FooBarQix.java
@@ -1,30 +1,45 @@
 package com.foobarqix.foobarqix;
 
+import java.util.HashMap;
+
 public class FooBarQix {
 
+    HashMap<Integer, String> map = new HashMap();
+
+    FooBarQix() {
+        map.put( 3, "Foo");
+        map.put( 5, "Bar");
+        map.put( 7, "Qix");
+    }
 
     public String print( Integer i ) {
         String result = "";
-        if ( i % 3 == 0 ) {
-            result += "Foo";
+        result += divisible(i);
+        result += contains(i);
+        if ( result.equals( "" ) ) {
+            result += i.toString();
         }
-        if ( i % 5 == 0 ) {
-            result += "Bar";
-        }
-        if ( i % 7 == 0 ) {
-            result += "Qix";
-        }
-        for ( String num : i.toString().split("") ) {
-            if ( num.contains("3") ) {
-                result += "Foo";
-            } else if ( num.contains("5") ) {
-                result += "Bar";
-            } else if ( num.contains("7") ) {
-                result += "Qix";
+        return result;
+    }
+
+    private String divisible(Integer i) {
+        String result = "";
+        for (Integer j : this.map.keySet()) {
+            if (i % j == 0) {
+                result += this.map.get(j);
             }
         }
-        if ( result == "") {
-            result += i.toString();
+        return result;
+    }
+
+    private String contains(Integer i) {
+        String result = "";
+        for ( String num : i.toString().split("") ) {
+            for (Integer j : this.map.keySet()) {
+                if ( num.contains(j.toString()) ) {
+                    result += this.map.get(j);
+                }
+            }
         }
         return result;
     }
