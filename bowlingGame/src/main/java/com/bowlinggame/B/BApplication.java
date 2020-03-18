a0fc84a784966656bf760eac8fe5c13dd2be8d47
Kevin <kevin.ng@online-pajak.com>
2020-03-18 16:43:16 +0800

    bowling game init

---
+++ bowlingGame/src/main/java/com/bowlinggame/B/BApplication.java
@@ -0,0 +1,13 @@
+package com.bowlinggame.B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BApplication.class, args);
+	}
+
+}
---
+++ bowlingGame/src/test/java/com/bowlinggame/B/BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowlinggame.B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
