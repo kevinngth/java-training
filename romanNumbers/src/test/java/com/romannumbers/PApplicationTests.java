e77d1162ce44176627bb7357d6240dd0dc6b7b25
Kevin <kevin.ng@online-pajak.com>
2020-03-18 11:35:42 +0800

    init romanNumbers

---
+++ romanNumbers/src/test/java/com/romannumbers/PApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romannumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
